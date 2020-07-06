db86694beb84489be19e43242ab1bc8e6cb09599
jason <jason.sinc95@gmail.com>
2020-07-06 15:16:48 +0000

    pipline test

--- script/docker.sh
+++ script/docker.sh
@@ -7,7 +7,7 @@ sudo apt-get remove docker docker-engine docker.io -y
 sudo apt-get purge containderd.io docker.io
 sudo apt-get install containerd.io
 sudo apt install docker.io -y
-sudo apt install docker-compse -y
+sudo apt install docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
