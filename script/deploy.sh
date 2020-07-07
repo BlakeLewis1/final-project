513c58dfb0dd11699c187af4abefcc968da147b1
Ubuntu <ubuntu@ip-10-0-1-174.eu-west-1.compute.internal>
2020-07-07 09:55:42 +0000

    deployment test

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,8 +2,8 @@
 . ~/.bashrc
 sudo apt install mysql-client-core-8.0
 
-mysql --host=34.105.175.39 --user=root --password=root < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/initDB.sql
+#mysql --host=34.105.175.39 --user=root --password=root < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/initDB.sql
 
-mysql --host=34.105.175.39 --user=root --password=root "petclinic" < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
+#mysql --host=34.105.175.39 --user=root --password=root "petclinic" < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
 
-kubectl apply -f /var/lib/jenkins/workspace/latte/kube-serv/k8_deployment.yml
+kubectl apply -f /home/ubuntu/final-project/k8_deployment.yml
