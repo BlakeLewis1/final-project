a4ce1f956e3187660b94432c6e111eaa79278b39
Ubuntu <ubuntu@ip-10-0-1-92.eu-west-1.compute.internal>
2020-07-07 13:24:58 +0000

    deployment test

--- script/kubernetes.sh
+++ script/kubernetes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
 curl -o kubectl.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl.sha256
 openssl sha1 -sha256 kubectl
 chmod +x ./kubectl
