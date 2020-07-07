5b50585f29e5a03043ed5d81294860624e0406b4
Ubuntu <ubuntu@ip-10-0-1-174.eu-west-1.compute.internal>
2020-07-07 10:14:10 +0000

    deployment test

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,4 +6,4 @@ sudo apt install mysql-client-core-8.0
 
 #mysql --host=34.105.175.39 --user=root --password=root "petclinic" < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
 
-kubectl apply -f /home/ubuntu/final-project/k8_deployment.yml
+kubectl apply -f /home/ubuntu/final-project/Kubernetes/k8_deployment.yml
