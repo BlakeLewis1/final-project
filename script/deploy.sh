0688eea214020168a21e2d3c0d54aadc50fd7544
Ubuntu <ubuntu@ip-10-0-1-92.eu-west-1.compute.internal>
2020-07-07 11:18:08 +0000

    deployment test

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,5 +5,5 @@ sudo apt install mysql-client-core-8.0
 #mysql --host=34.105.175.39 --user=root --password=root < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/initDB.sql
 
 #mysql --host=34.105.175.39 --user=root --password=root "petclinic" < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
-
+aws eks update-kubeconfig --name t3_eks
 kubectl apply -f /home/ubuntu/final-project/Kubernetes/k8_deployment.yml
