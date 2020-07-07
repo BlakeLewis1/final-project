0e371f70ce800c87e1c845fbf0a684840d11b32b
Ubuntu <ubuntu@ip-10-0-1-174.eu-west-1.compute.internal>
2020-07-07 10:18:04 +0000

    deployment test

--- script/kubernetes.sh
+++ script/kubernetes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
 curl -o kubectl.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl.sha256
 openssl sha1 -sha256 kubectl
 chmod +x ./kubectl
@@ -8,4 +8,3 @@ sudo mv ./kubectl /usr/local/bin
 mkdir -p $HOME/bin && mv ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --short --client
-aws eks update-kubeconfig --name PetClinic
