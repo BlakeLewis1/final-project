22b42e6d2542a8f130769e16dbd4f7c919174aae
Ubuntu <ubuntu@ip-10-0-1-92.eu-west-1.compute.internal>
2020-07-07 11:39:00 +0000

    deployment test

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,4 +6,9 @@ sudo apt install mysql-client-core-8.0
 
 #mysql --host=34.105.175.39 --user=root --password=root "petclinic" < /home/jaesinc/final-project/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
 aws eks update-kubeconfig --name t3_eks
-kubectl apply -f /home/ubuntu/final-project/Kubernetes/k8_deployment.yml
+
+kubectl apply -f /home/ubuntu/final-project/Kubernetes/k8_nginx.yaml
+sleep 20
+kubectl apply -f /home/ubuntu/final-project/Kubernetes/k8_backend.yaml
+
+kubectl apply -f /home/ubuntu/final-project/Kubernetes/k8_frontend.yaml
